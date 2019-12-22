589c8464f489aafcaf65b6d2a82486ea67815655
storyxc <andresiniesta@foxmail.com>
2019-12-22 19:19:55 +0800

    前端history模式适配

--- storyxc_service/src/main/java/com/storyxc/StoryXcApplication.java
+++ storyxc_service/src/main/java/com/storyxc/StoryXcApplication.java
@@ -6,6 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 /**
  * @author Xc
@@ -19,5 +22,15 @@ public class StoryXcApplication {
     public static void main(String[] args) {
         SpringApplication.run(StoryXcApplication.class,args);
         logger.info("启动成功");
+    }
+
+    @Bean
+    public ErrorPageRegistrar errorPageRegistrar(){
+        return new ErrorPageRegistrar() {
+            @Override
+            public void registerErrorPages(ErrorPageRegistry registry) {
+                registry.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
+            }
+        };
     }
 }
