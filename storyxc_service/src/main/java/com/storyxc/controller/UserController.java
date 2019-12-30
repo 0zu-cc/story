798e4a10d47219d6c5e480832b28ebd7fe4e3b11
storyxc <andresiniesta@foxmail.com>
2019-12-30 18:24:12 +0800

    add

--- storyxc_service/src/main/java/com/storyxc/controller/UserController.java
+++ storyxc_service/src/main/java/com/storyxc/controller/UserController.java
@@ -5,6 +5,8 @@ import com.storyxc.entity.StatusCode;
 import com.storyxc.pojo.User;
 import com.storyxc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -18,6 +20,17 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/loginStatus")
+    public Result loginStatus(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        String userName = principal.toString();
+        if ("anonymousUser".equals(userName)){
+            return new Result(false,StatusCode.OK,"用户未登录");
+        }
+        return new Result(true,StatusCode.OK,"查询登录状态成功",userName);
+    }
 
     @PostMapping
     public Result addUser(@RequestBody User user, Integer[] roleIds) {
