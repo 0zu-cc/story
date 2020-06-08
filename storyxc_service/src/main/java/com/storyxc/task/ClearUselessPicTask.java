05a96ded65533a6a95aac7f1c61ef91cb20777c7
story <andresiniesta@foxmail.com>
2020-06-08 23:16:47 +0800

    取消图片清理

--- storyxc_service/src/main/java/com/storyxc/task/ClearUselessPicTask.java
+++ storyxc_service/src/main/java/com/storyxc/task/ClearUselessPicTask.java
@@ -41,25 +41,25 @@ public class ClearUselessPicTask {
         try {
 
             logger.info("开始执行清理无用文章图片定时任务");
-            Set<String> articleIds = redisTemplate.boundHashOps("article_pic").keys();
-            Set<String> usefulPics = new HashSet<>();
-            for (String articleId : articleIds) {
-                String setString = (String) redisTemplate.boundHashOps("article_pic").get(articleId);
-                usefulPics.addAll(JSON.parseArray(setString,String.class));
-            }
-            redisTemplate.boundSetOps("tempUsefulPics").add(usefulPics.toArray());
-            Set tempUsefulPics = redisTemplate.boundSetOps("tempUsefulPics").members();
-            Set<String> diff = redisTemplate.boundSetOps("articlePicSet").diff("tempUsefulPics");
-            logger.info("无用图片共[{}]个,图片列表:[{}],", diff.size(), diff);
-            String[] pics2Delete = diff.toArray(new String[]{});
-            long count = 0;
-            if (diff.size() > 0) {
-                qiNiuUtils.removeFiles(pics2Delete);
-                count = redisTemplate.boundSetOps("articlePicSet").remove(diff.toArray());
-            }
-            redisTemplate.delete("tempUsefulPics");
-            logger.info("清理无用文章图片任务执行完毕,共清理无用文件[{}]个", count);
-            logger.info("删除临时缓存usefulPics-[{}]", tempUsefulPics);
+//            Set<String> articleIds = redisTemplate.boundHashOps("article_pic").keys();
+//            Set<String> usefulPics = new HashSet<>();
+//            for (String articleId : articleIds) {
+//                String setString = (String) redisTemplate.boundHashOps("article_pic").get(articleId);
+//                usefulPics.addAll(JSON.parseArray(setString,String.class));
+//            }
+//            redisTemplate.boundSetOps("tempUsefulPics").add(usefulPics.toArray());
+//            Set tempUsefulPics = redisTemplate.boundSetOps("tempUsefulPics").members();
+//            Set<String> diff = redisTemplate.boundSetOps("articlePicSet").diff("tempUsefulPics");
+//            logger.info("无用图片共[{}]个,图片列表:[{}],", diff.size(), diff);
+//            String[] pics2Delete = diff.toArray(new String[]{});
+//            long count = 0;
+//            if (diff.size() > 0) {
+//                qiNiuUtils.removeFiles(pics2Delete);
+//                count = redisTemplate.boundSetOps("articlePicSet").remove(diff.toArray());
+//            }
+//            redisTemplate.delete("tempUsefulPics");
+//            logger.info("清理无用文章图片任务执行完毕,共清理无用文件[{}]个", count);
+//            logger.info("删除临时缓存usefulPics-[{}]", tempUsefulPics);
         } catch (RedisSystemException e) {
             logger.warn("清理图片任务失败");
         }
